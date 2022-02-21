a2d0821872173179ecd6f29ed5ae691d4cab0dbd
samira <sami.younesy@gmail.com>
2022-02-21 16:19:15 +0100

    buttons

--- reseau_achats/src/App.js
+++ reseau_achats/src/App.js
@@ -1,22 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: calc(10px + 2vmin);
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const {loading, }
+
+function clickMe() {
+  this.setState({ loading: true }, () => {
+    myrequest()
+    .then(result => this.setState({
+        loading: false,
+        word : result
+      }),
+      );
+  });
+}
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+function myrequest(){
+  sleep(500)
+  return "ok";
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p> SQL<br></br>
+          <Button onClick={clickMe}>Requête 1</Button>
+          <Button onClick={clickMe}>Requête 2</Button>
+          <Button onClick={clickMe}>Requête 3</Button>
+          {loading ? <p>Chargement</p> : <p>Ok !</p>}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <br></br>
+        <br></br>
+        <p> NoSQL<br></br>
+          <Button onClick={clickMe}>Requête 1</Button>
+          <Button onClick={clickMe}>Requête 2</Button>
+          <Button onClick={clickMe}>Requête 3</Button>
+        </p>
+        
       </header>
     </div>
   );
